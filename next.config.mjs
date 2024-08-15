845e6f6ef85ac76f7c674e190934a8a0aa84ee71
seki <102042736+HERUESTA@users.noreply.github.com>
2024-08-15 19:35:10 +0900

    URL変更

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    NEXT_PUBLIC_PARENT_DOMAIN: process.env.NEXT_PUBLIC_PARENT_DOMAIN,
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  },
+};
 
 export default nextConfig;
