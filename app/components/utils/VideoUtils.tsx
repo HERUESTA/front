5cba682d6a0c7b4bd0519c6d44539d986ae6108a
seki <102042736+HERUESTA@users.noreply.github.com>
2024-09-14 20:15:10 +0900

    いいね画面の実装

--- app/components/utils/VideoUtils.tsx
+++ app/components/utils/VideoUtils.tsx
@@ -1,10 +1,12 @@
-import { Video, TwitchResponse } from "@/app/type/api/video";
+// app/components/utils/VideoUtils.tsx
+
+import { Video, TwitchResponse } from '../../type/api/video';
 
 export const processVideoData = (response: TwitchResponse): Video[] => {
-  return response.data.map((video: Video) => ({
+  return response.data.map((video: any) => ({
     id: video.id,
     url: `https://www.twitch.tv/videos/${video.id}`,
     title: video.title,
-    thumbnail_url: video.thumbnail_url,
+    thumbnail_url: video.thumbnail_url.replace('%{width}x%{height}', '320x180'),
   }));
 };
