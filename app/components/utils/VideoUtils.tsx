ca63dad6099280acc3d604214d8777127a071636
seki <102042736+HERUESTA@users.noreply.github.com>
2024-09-14 20:39:30 +0900

    いいね

--- app/components/utils/VideoUtils.tsx
+++ app/components/utils/VideoUtils.tsx
@@ -1,10 +1,12 @@
-import { Video, TwitchResponse, TwitchVideoData } from '../../type/api/video';
+// app/components/utils/VideoUtils.tsx
+
+import { Video, TwitchResponse } from '../../type/api/video';
 
 export const processVideoData = (response: TwitchResponse): Video[] => {
-  return response.data.map((video: TwitchVideoData) => ({
+  return response.data.map((video: any) => ({
     id: video.id,
     url: `https://www.twitch.tv/videos/${video.id}`,
     title: video.title,
-    thumbnail_url: video.thumbnail_url.replace('%{width}x%{height}', '320x180'),
+    thumbnail_url: video.thumbnail_url || "", // thumbnail_urlのプロパティを追加、デフォルトで空文字列に
   }));
 };
