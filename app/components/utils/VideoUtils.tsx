8e8213a798fb3bf78f7fad07f1a1e312acbfd3a5
seki <102042736+HERUESTA@users.noreply.github.com>
2024-09-14 20:27:56 +0900

    いいね

--- app/components/utils/VideoUtils.tsx
+++ app/components/utils/VideoUtils.tsx
@@ -7,6 +7,6 @@ export const processVideoData = (response: TwitchResponse): Video[] => {
     id: video.id,
     url: `https://www.twitch.tv/videos/${video.id}`,
     title: video.title,
-    thumbnail_url: video.thumbnail_url.replace('%{width}x%{height}', '320x180'),
+    thumbnail_url: video.thumbnail_url, // 追加
   }));
 };
